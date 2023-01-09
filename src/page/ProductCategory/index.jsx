3c26d6e560879366176825f287f729ac82c0b210
Paulinha <paulafabianasoares@gmail.com>
2023-01-09 18:46:39 -0300

    pasta components, pasta page e configuração das rotas

---
+++ src/page/ProductCategory/index.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const ProductCategory = () => {
+  return (
+    <div>Category</div>
+  )
+}
---
+++ src/page/index.js
@@ -0,0 +1,5 @@
+export * from './Home/index';
+export * from './Cart/index';
+export * from './PageNotFound/index';
+export * from './ProductCategory/index';
+export * from './ProductDetails/index';
